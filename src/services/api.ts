ef8fda5625ec8c06649733455677f217f0f3313b
Jean Caetano <caetanojean4@gmail.com>
2025-11-23 15:59:31 -0300

    FIX: Atualizando o link da api para o do deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+// Link do local: http://192.168.18.38:3000
 // link da vercel: https://mobile-backend-nu.vercel.app
-export const API_URL = 'http://192.168.18.38:3000';
+export const API_URL = 'https://mobile-backend-nu.vercel.app';
 
 const api = axios.create({
     baseURL: API_URL,
